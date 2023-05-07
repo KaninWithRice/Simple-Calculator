177ab0492540a3c7079baf40e805c1c3ee7aeda6
KaninWithRice <mishalnorona014@gmail.com>
2023-05-07 21:07:21 +0800

    Pseudocode

---
+++ Simple_Calc.py
@@ -0,0 +1,13 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Simple Calculator
+# Import Color Module
+# add introduction
+# Addition Function
+# Subtraction Function
+# Multiplication Function
+# Division Fuction with ZeroDivisionError
+# List down Operatios
+# Ask for user's Input
+# Ask for user's Input Numbers
+# Add a ValueError
+# Print the out put
+# Ask if the user's wants to try again
